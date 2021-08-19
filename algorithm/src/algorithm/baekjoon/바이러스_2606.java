6afc63d10417bc7b5e8f52be9a9f13a229112422
JSpark <wlstjd-0-@hanmail.net>
2021-08-19 23:13:31 +0900

    test

--- algorithm/src/algorithm/baekjoon/바이러스_2606.java
+++ algorithm/src/algorithm/baekjoon/바이러스_2606.java
@@ -1,14 +1,26 @@
 package algorithm.baekjoon;
 
 import java.io.BufferedReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 import java.util.Queue;
 import java.util.Set;
 
 public class 바이러스_2606 {
 
     public static void main(String[] args) {
+	Map<String, String> test = new HashMap<>();
+	Set<String> tt = new HashSet<>();
+	List<String> tert = new ArrayList<>();
+	String str = "aa";
+	str.toCharArray();
+
+	Collections.sort(tert);
 	try (BufferedReader br = new BufferedReader(new java.io.InputStreamReader(System.in))) {
 	    int size = Integer.parseInt(br.readLine()) + 1;
 	    int[][] computerMap = new int[size][size];
